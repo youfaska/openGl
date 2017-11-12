a22ba69e1ee0db9ec09dd5cd60697c7cac99ab70
DESKTOP-HKDF74C\youfaska <youssef.oufaska@gmail.com>
2017-11-12 21:25:47 +0100

    dibujar una corcunferencia

--- solution.c
+++ solution.c
@@ -3,7 +3,17 @@
 #include "solution.h"
  
 void draw_hoop(GLuint center_x, GLuint center_y, GLuint radius) {
- 
+	float calx = 0.0, caly = 0.0;
+ 	//Dibujar una circunferencia
+	glColor3f(1.0,0.0,1.0); // rojo verde azul
+	glBegin(GL_POINTS);
+	double i = 0.0;
+	for ( i = 0.0; i < 10 ; i+=0.001  ){
+		calx = radius * cos(i);
+		caly = radius * sin(i);
+		glVertex2f(calx,caly);
+	}
+	glEnd();	
 }
 void draw_marks(GLuint center_x, GLuint center_y, GLuint radius) {
  
