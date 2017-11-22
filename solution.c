77bda856a4c84695188b664e5f7602854d02002c
DESKTOP-HKDF74C\youfaska <youssef.oufaska@gmail.com>
2017-11-22 22:44:07 +0100

    comenzar la actividad 2

--- solution.c
+++ solution.c
@@ -1,47 +1,31 @@
-#include <math.h>
 #include "solution.h"
-#include <stdio.h>
- 
-/*Dibujar una circunferencia*/ 
-void draw_hoop(GLuint center_x, GLuint center_y, GLuint radius) {
-	float calx = 0.0, caly = 0.0;
- 
-	glColor3f(1.0,0.0,1.0); // rojo, verde, azul
-	glBegin(GL_LINE_LOOP); // hay que usar el loop para enlace con todos los puntos, si no deja huecos...
-	//glBegin(GL_LINES);
-	double i = 0.0, angulo = 0.0;
-	double max = 0.5;
-	for ( i = 0; i < max ; i+=0.01  ){ // vamos dibujando pixel a pixel
-		angulo = 2*M_PI*i/max;
-		calx = radius * cos(angulo);
-		caly = radius * sin(angulo);
-		glVertex2f(calx,caly);
-	}
-	glEnd();
+#include <assert.h>
+
+#define LEFT 8
+#define RIGHT 4
+#define BOTTOM 2
+#define TOP 1
+
+int LRBT(Point* p, GLfloat wxmin, GLfloat wymin, GLfloat wxmax, GLfloat wymax) {
+  return 0;
+}
+
+void cut_p1_side(Point* p1, const Point* p2, GLfloat wxmin, GLfloat wymin, GLfloat wxmax, GLfloat wymax) {
 
 }
-/* Dibujar las marcas del reloj */
-void draw_marks(GLuint center_x, GLuint center_y, GLuint radius) {
-	glColor3f(1.0,0.0,1.0); // rojo, verde, azul
-	glBegin(GL_LINES); // dibujamos  la linea, para ello necesitamos con GL_LINES tener dos puntos...
-	int separacionCircunferencia = 6;
-	glVertex2i(radius-separacionCircunferencia,center_y);glVertex2i(radius,center_y);
-	glVertex2i(center_x,radius-separacionCircunferencia);glVertex2i(center_x,radius);
-	glVertex2i(center_x,separacionCircunferencia-radius);glVertex2i(center_x,-radius);
-	glVertex2i(-radius+separacionCircunferencia,center_y);glVertex2i(-radius,center_y);
-	glEnd();
- //ggg
+
+/*
+ * Returns a boolean indicating if the line is completely discarded, or not
+ */
+int cohen_sutherland(Point* p1, Point* p2, GLfloat wxmin, GLfloat wymin, GLfloat wxmax, GLfloat wymax) {
+  return 0;
 }
-void draw_hands(GLuint center_x, GLuint center_y, GLuint radius, GLuint hour, GLuint minute) {
- 	glColor3f(0.0,0.0,0.0); // rojo, verde, azul
- 	glLineWidth(2.0);
-	glBegin(GL_LINES);
-	int aumentarTamanio = 90;
-	glVertex2i(center_x,center_x);glVertex2i(minute+aumentarTamanio,center_y);
-	glEnd();
-	aumentarTamanio = aumentarTamanio + 30;
-	glLineWidth(1.0);
-	glBegin(GL_LINES);
-	glVertex2i(center_x,center_y);glVertex2i(center_x,hour+aumentarTamanio);
-	glEnd();
+
+/*
+ * Returns a boolean indicating if the line is completely discarded, or not
+ */
+int sutherland_hodgman(Point* polygon,int* p_n_vertices, GLfloat wxmin, GLfloat wymin, GLfloat wxmax, GLfloat wymax) {
+  return 0;
 }
+
+
