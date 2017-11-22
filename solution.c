e9b50b4a770801220ffd1a52a6b2c506692a3459
DESKTOP-HKDF74C\youfaska <youssef.oufaska@gmail.com>
2017-11-22 20:25:20 +0100

    Con el bucle del profesor usando PI para conseguir el angulo polar

--- solution.c
+++ solution.c
@@ -1,19 +1,20 @@
-
 #include <math.h>
 #include "solution.h"
 #include <stdio.h>
  
 /*Dibujar una circunferencia*/ 
 void draw_hoop(GLuint center_x, GLuint center_y, GLuint radius) {
-
 	float calx = 0.0, caly = 0.0;
  
 	glColor3f(1.0,0.0,1.0); // rojo, verde, azul
-	glBegin(GL_POINTS);
-	double i = 0.0;
-	for ( i = 0.0; i < 10 ; i+=0.001  ){ // vamos dibujando pixel a pixel
-		calx = radius * cos(i);
-		caly = radius * sin(i);
+	glBegin(GL_LINE_LOOP); // hay que usar el loop para enlace con todos los puntos, si no deja huecos...
+	//glBegin(GL_LINES);
+	double i = 0.0, angulo = 0.0;
+	double max = 0.5;
+	for ( i = 0; i < max ; i+=0.01  ){ // vamos dibujando pixel a pixel
+		angulo = 2*M_PI*i/max;
+		calx = radius * cos(angulo);
+		caly = radius * sin(angulo);
 		glVertex2f(calx,caly);
 	}
 	glEnd();
@@ -35,9 +36,11 @@ void draw_hands(GLuint center_x, GLuint center_y, GLuint radius, GLuint hour, GL
  	glColor3f(0.0,0.0,0.0); // rojo, verde, azul
  	glLineWidth(2.0);
 	glBegin(GL_LINES);
-	int aumentarTamanio = 20;
+	int aumentarTamanio = 50;
 	glVertex2i(center_x,center_x);glVertex2i(minute+aumentarTamanio,center_y);
+	glEnd();
 	glLineWidth(1.0);
+	glBegin(GL_LINES);
 	glVertex2i(center_x,center_y);glVertex2i(center_x,hour+aumentarTamanio);
 	glEnd();
 }
