728259f0391a41bb67c21893f1c7cb7623dee8f1
DESKTOP-HKDF74C\youfaska <youssef.oufaska@gmail.com>
2017-11-22 22:48:52 +0100

    comenzamos la actividad 2

--- main.c
+++ main.c
@@ -8,19 +8,21 @@
 #define WINDOW_WIDTH 640
 #define WINDOW_HEIGHT 480
 #define MARGIN_WIDTH 40
- 
+
 GLint WindowWidth = WINDOW_WIDTH;
 GLint WindowHeight = WINDOW_HEIGHT;
- 
+
+
 void init_gl(int argc, char * argv[]) {
   glutInit(&argc, argv);
   glutInitDisplayMode(GLUT_RGB);
   glutInitWindowPosition(50, 50);
   glutInitWindowSize(WindowWidth, WindowHeight);
   glutCreateWindow(argv[0]);
-  glClearColor(1.0, 1.0, 1.0, 1.0);
+  glClearColor(0.0, 0.0, 0.0, 1.0);
+  glEnable(GL_LINE_SMOOTH);
 }
- 
+
 void reshape_func(GLint width, GLint height) {
   glViewport(0, 0, WindowWidth=width, WindowHeight=height);
   glMatrixMode(GL_PROJECTION);
@@ -29,24 +31,67 @@ void reshape_func(GLint width, GLint height) {
   glMatrixMode(GL_MODELVIEW);
   glLoadIdentity();
 }
- 
+
+void draw_line(Point* p1, Point*p2, GLfloat width, GLfloat red, GLfloat green, GLfloat blue) {
+  glColor3f(red, green, blue);
+  glLineWidth(width);
+  glBegin(GL_LINES);
+  glVertex2fv((GLfloat*)p1);
+  glVertex2fv((GLfloat*)p2);
+  glEnd();
+}
+
+void draw_cohen_sutherland() {
+  // Draw clipping window
+  GLfloat wxmin=-230, wymin=-70, wxmax=-50, wymax=70;
+  glColor3f(0.0f, 0.3f, 1.0f);
+  glRectf(wxmin, wymin, wxmax, wymax);
+  // Draw line
+  Point p1={-280,100};
+  Point p2={-150,-100};
+  draw_line(&p1, &p2, 1.0f, 0.1f, 1.0f, 0.0f);
+  // Draw clipped line
+  int discarded = cohen_sutherland(&p1,&p2,wxmin,wymin,wxmax,wymax);
+  if (!discarded) {
+    draw_line(&p1, &p2, 5.0f, 1.0f, 0.1f, 0.0f);
+  }
+}
+
+void draw_polygon(Point* vertices, int n_vertices, GLfloat width, GLenum mode, GLfloat red, GLfloat green, GLfloat blue) {
+  glPolygonMode(GL_FRONT, mode);
+  glColor3f(red, green, blue);
+  glLineWidth(width);
+  glBegin(GL_POLYGON);
+  int i;
+  for (i=0;i<n_vertices;i++) {
+    glVertex2fv((GLfloat*)vertices++);
+  }
+  glEnd();
+}
+
+void draw_sutherland_hodgman() {
+  GLfloat wxmin=50, wymin=-70, wxmax=230, wymax=70;
+  glColor3f(0.0f, 0.3f, 1.0f);
+  glRectf(wxmin, wymin, wxmax, wymax);
+  // Draw polygon
+  Point polygon[MAX_POLYGON_VERTICES] = {{70,120}, {160,-120}, {280,0}};
+  int n_vertices = 3;
+  draw_polygon(polygon, n_vertices, 1, GL_LINE, 0.0f, 0.9f, 0.2f);
+  // Draw clipped polygon
+  int discarded = sutherland_hodgman(polygon,&n_vertices,wxmin,wymin,wxmax,wymax);
+  if (!discarded) {
+    draw_polygon(polygon, n_vertices, 1, GL_FILL, 1.0f, 0.1f, 0.0f);
+  }
+}
+
 void display_func() {
   glClear(GL_COLOR_BUFFER_BIT);
   glMatrixMode(GL_MODELVIEW);
-  // Compute data
-  GLuint radius = (WindowWidth<WindowHeight)?WindowWidth:WindowHeight;
-  radius = (radius/2) - MARGIN_WIDTH;
-  time_t rawtime;
-  struct tm *timeinfo;
-  time ( &rawtime );
-  timeinfo = localtime (&rawtime);
-  // Draw the clock
-  draw_hoop(0, 0, radius);
-  draw_marks(0, 0, radius);
-  draw_hands(0, 0, radius, timeinfo->tm_hour, timeinfo->tm_min);
+  draw_cohen_sutherland();
+  draw_sutherland_hodgman();
   glFlush();
 }
- 
+
 int main(int argc, char * argv[]) {
   init_gl(argc, argv);
   glutReshapeFunc(reshape_func);
@@ -54,3 +99,4 @@ int main(int argc, char * argv[]) {
   glutMainLoop();
   return 0;
 }
+
--- solution.h
+++ solution.h
@@ -1,11 +1,24 @@
 #ifndef _SOLUTION_H_
 #define _SOLUTION_H_
- 
+
 #include <GL/GL.h>
 #include <GL/GLUT.h>
- 
-void draw_hoop(GLuint center_x, GLuint center_y, GLuint radius);
-void draw_marks(GLuint center_x, GLuint center_y, GLuint radius);
-void draw_hands(GLuint center_x, GLuint center_y, GLuint radius, GLuint hour, GLuint minute);
- 
+
+#define MAX_POLYGON_VERTICES 256
+
+typedef struct _Point {
+  GLfloat x;
+  GLfloat y;
+} Point;
+
+// Cohen-Sutherland
+int LRBT(Point* p, GLfloat wxmin, GLfloat wymin, GLfloat wxmax, GLfloat wymax);
+void cut_p1_side(Point* p1, const Point* p2, GLfloat wxmin, GLfloat wymin, GLfloat wxmax, GLfloat wymax);
+int cohen_sutherland(Point* p1, Point* p2, GLfloat wxmin, GLfloat wymin, GLfloat wxmax, GLfloat wymax);
+
+
+// Sutherland-Hodgman
+int sutherland_hodgman(Point* polygon,int* p_n_vertices, GLfloat wxmin, GLfloat wymin, GLfloat wxmax, GLfloat wymax);
+
 #endif
+
